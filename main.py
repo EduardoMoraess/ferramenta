82cab6c9c5c92e03f0a57d87cf4a1d2e63dff2f1
EduardoMoraess <eduzinho2118@gmail.com>
2025-09-23 21:10:03 -0300

    adadsad

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import secrets
 import string
 
-def gerar_senha_numerica():
-    nome = input("Digite seu nome: ").strip()
-    # Conta apenas letras
+def gerar_senha_numericas():
+    nome = input("Digite seu nome =>")
+
     tamanho = sum(1 for letra in nome if letra.isalpha())
 
-    if tamanho < 1:
-        print("Nome inválido! Vou usar senha de 3 números como padrão.")
-        tamanho = 3
-
-    # Gera senha apenas com números aleatórios
+    if tamanho < 1 :
+        print('Nome invalido!')
+    
     senha = ''.join(secrets.choice(string.digits) for _ in range(tamanho))
     return senha
-
 if __name__ == "__main__":
-    senha_gerada = gerar_senha_numerica()
+    senha_gerada = gerar_senha_numericas()
     print("Senha gerada:", senha_gerada)
